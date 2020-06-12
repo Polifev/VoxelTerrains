3504fb67eaf5359a7a5ad6e13875eac1efe4c5b9
Polifev <pol007@skynet.be>
2020-06-12 14:37:55 +0200

    Implementation of a linear interpolation chunk

---
+++ Assets/Scripts/Source/ScalarField/ChunkBasedScalarField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VoxelTerrains.ScalarField
+{
+    public class ChunkBasedScalarField : AbstractScalarField
+    {
+        public override float ValueAt(Vector3 vector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
